f4c37ff80f5c7b719c7e747bd9bce87455ad096d
Sean Hickey <sean@ventcore.org>
2020-12-29 14:40:52 -0600

    Removed SPO tenant module in favor of PnP only

--- M365/Get-OD4BUsage.ps1
+++ M365/Get-OD4BUsage.ps1
@@ -1,10 +1,15 @@
 ###
-#   Get-OD4BUsage: quick & dirty script to analyze breakdown 
+#   Get-OD4BUsage: second draft of a script to analyze breakdown 
 #   of file type vs. size/count in all user OneDrives in an O365 tenant
-#   as with any script querying user data, use with approval and be mindful
-#   of ethical and legal considerations!
+#   as with any script querying user data, use with clear & direct approval 
+#   and be mindful of ethical and legal considerations!
 #   Could be easily adapted for SPO sites instead.
+#   Changes:
+#           12/29/2020:
+#           -Converted to use PnP module to query sites, so that the SPO tenant module doesn't need to be used (simplifies authentication)
+#           -Tested with the new PnP module and PowerShell 7 only
 #   Likely improvements: 
+#           -convert to a module to separate processing of individual sites (in module) from selection of sites (in script)
 #           -move file type configuration to external source
 #           -test performance against larger tenant and improve from there
 #           -switch to Azure AD app authentication
@@ -17,7 +22,8 @@
 #   Variables / Configuration
 ###
 
-$tenant = "ventcore"
+$tenantName = "ventcore"
+$tenantDomain = "ventcore.org"
 $divisor = "1MB" #file size will be divided by this
 
 $path = "$HOME\Desktop\OD4B Reports\"
@@ -26,14 +32,14 @@ $fullPath = $path + ($filename -f (Get-Date).ToShortDateString().Replace("/","-"
 
 $extensionTypes = @{}
 
-$extensionTypes.Add("Word",@("doc","docx","txt"))
+$extensionTypes.Add("Text",@("doc","docx","txt"))
 $extensionTypes.Add("Images",@("cr2","jpg"))
 $extensionTypes.Add("Spreadsheets",@("xls","xlsx","ods"))
 $extensionTypes.Add("PDF",@("pdf"))
 $extensionTypes.Add("Scripts",@("ps1","psm1","bat","py"))
 $extensionTypes.Add("Code",@("cs","resx","settings","csproj","pdb","cache","sln","manifest","resources","suo"))
 $extensionTypes.Add("Music",@("mp3","ogg"))
-$extensionTypes.Add("Video",@("mp4","mov","avi","mpg","mpeg"))
+$extensionTypes.Add("Video",@("mp4","mov","avi","mpg","mpeg","mkv"))
 $extensionTypes.Add("Data",@("xml"))
 $extensionTypes.Add("Archives",@("zip","rar"))
 $extensionTypes.Add("Passwords",@("kdbx"))
@@ -42,22 +48,18 @@ $extensionTypes.Add("Other",@("thm"))
 
 ###
 #   Check for connection/connect to SPO and input PnP credentials
-#   AppID/Secret created as per 
-#   https://docs.microsoft.com/en-us/sharepoint/dev/solution-guidance/security-apponly-azureacs
-#   With read-only tenant-wide access
+#   AppID/thumbprint created as per 
+#   https://docs.microsoft.com/en-us/powershell/module/sharepoint-pnp/initialize-pnppowershellauthentication?view=sharepoint-ps
+#   With default permissions, but those could/should be downgraded to read-only unless needed for other scripts
 ###
 
-try
-{
-    Get-SPOTenant | Out-Null
-}
-catch
-{
-    Connect-SPOService -url https://$tenant-admin.sharepoint.com
-}
+$appID = $null
+$thumbprint = $null
 
-$appID = Read-Host "Enter the AppID"
-$appSecret = Read-Host "Enter the App Secret"
+if($null -eq $appID -or $null -eq $thumbprint)
+{
+    throw "Provision and set appID and thumbprint values in script, config file, or import from desired credential store before using."
+}
 
 ###
 #   Load users
@@ -65,7 +67,12 @@ $appSecret = Read-Host "Enter the App Secret"
 
 $allUserData = @{}
 
-$manyDrives = Get-SPOSite -IncludePersonalSite $true -Template "SPSPERS#10" -Limit All
+#$manyDrives = Get-SPOSite -IncludePersonalSite $true -Template "SPSPERS#10" -Limit All
+
+Connect-PnPOnline -Url https://$tenantName-admin.sharepoint.com -ClientID $appID -Thumbprint $thumbprint -Tenant $tenantDomain
+$list = "DO_NOT_DELETE_SPLIST_TENANTADMIN_ALL_SITES_AGGREGATED_SITECOLLECTIONS"
+$manyDrives = Get-PnPListItem -List $list -Fields TemplateName,SiteUrl | where {$_.FieldValues["TemplateName"] -eq "SPSPERS#10"}
+
 $userCount = 1
 
 foreach($oneDrive in $manyDrives)
@@ -82,11 +89,11 @@ foreach($oneDrive in $manyDrives)
     $userData.Add("TotalCount",0)
     $userData.Add("TotalSize",[long]0)
 
-    Connect-PnPOnline -Url $oneDrive.Url -ClientID $appID -ClientSecret $appSecret
+    Connect-PnPOnline -Url $oneDrive.FieldValues["SiteUrl"] -ClientID $appID -Thumbprint $thumbprint -Tenant $tenantDomain
     
     #check url is really connected to avoid non-blocking PnP errors/failures
     $site = Get-PnPSite
-    if($oneDrive.Url -ne $site.Url)
+    if($oneDrive.FieldValues["SiteUrl"] -ne $site.Url)
     {
         Write-Warning ("Failed to connect to " + $site.Url)
     }
@@ -136,7 +143,7 @@ foreach($oneDrive in $manyDrives)
             $libraryCount = $libraryCount + 1
         }
 
-        $allUserData.Add($oneDrive.Owner, $userData)
+        $allUserData.Add($oneDrive.FieldValues["SiteUrl"], $userData)
     }
 
     $userCount = $userCount + 1
